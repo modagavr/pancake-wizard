994ab4f954bfdb952e18bc80be785a6165173b98
Egor Gavrilov <modagavr@gmail.com>
2021-06-09 17:30:08 +0300

    Hello 😼💸

---
+++ README.md
@@ -0,0 +1,23 @@
+# 🎰 PancakeSwap is afraid of this! This bot is a casino winner
+
+This bot wins almost every 5 minute BNB-USD option on PancakeSwap Prediction
+
+![alt PancakeSwap Prediciton Bot-Winner](ppw-image.png)
+
+## 💡 How to use
+
+1. Rename **.env.example** to **.env** `mv .env.example .env`
+2. Provide your private key to **.env** PRIVATE_KEY field
+3. `node index.js`
+4. Enjoy winning!
+
+### Feel free to contribute. Good Luck!
+
+## 💥 Disclaimer
+
+All investment strategies and investments involve risk of loss.
+**Nothing contained in this program, scripts, code or repository should be construed as investment advice.**
+Any reference to an investment's past or potential performance is not,
+and should not be construed as, a recommendation or as a guarantee of
+any specific outcome or profit.
+By using this program you accept all liabilities, and that no claims can be made against the developers or others connected with the program.
---
+++ index.js
@@ -0,0 +1,76 @@
+// Global Config
+const BNBPP_ADDRESS = "0x516ffd7d1e0ca40b1879935b2de87cb20fc1124b";
+const AMOUNT_TO_BET = "0.1"; // in BNB
+
+import dotenv from "dotenv";
+
+import { formatEther, parseEther } from "@ethersproject/units";
+import { ethers } from "ethers";
+
+import { BNBPP_ABI } from "./bnbpp-abi.js";
+
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed.binance.org/"
+);
+
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+const bnbppContract = new ethers.Contract(BNBPP_ADDRESS, BNBPP_ABI, signer);
+
+// Utility Function to use **await sleep(ms)**
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+bnbppContract.on("StartRound", async (epoch) => {
+  console.log("Started Epoch", +epoch);
+
+  // Waiting for 282sec = 4.7min
+  await sleep(282000);
+
+  console.log("Getting Amounts");
+
+  const { bullAmount, bearAmount } = await bnbppContract.rounds(epoch);
+
+  console.log("Bull Amount", formatEther(bullAmount), "BNB");
+  console.log("Bear Amount", formatEther(bearAmount), "BNB");
+
+  // Betting on Bear if BullAmount > BearAmount
+  const bearBet = +bullAmount > +bearAmount;
+
+  console.log("Betting on", bearBet ? "Bear" : "Bull");
+
+  console.log("Betting is Started");
+
+  if (bearBet) {
+    const tx = await bnbppContract.betBear({
+      value: parseEther(AMOUNT_TO_BET),
+    });
+
+    console.log("Bear Betting Tx Started");
+
+    try {
+      await tx.wait();
+
+      console.log("Bear Betting Tx Success");
+    } catch {
+      console.log("Bear Betting Tx Error");
+    }
+  } else {
+    const tx = await bnbppContract.betBull({
+      value: parseEther(AMOUNT_TO_BET),
+    });
+
+    console.log("Bull Betting Tx Started");
+
+    try {
+      await tx.wait();
+
+      console.log("Bull Betting Tx Success");
+    } catch {
+      console.log("Bull Betting Tx Error");
+    }
+  }
+});
