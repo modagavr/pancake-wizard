bc32149e6be43ae7860fa8bf101e6161ecebf7a7
Egor Gavrilov <modagavr@gmail.com>
2021-11-07 19:12:58 +0300

    TURBO MODE CLI

--- src/candlegenie-btc.ts
+++ src/candlegenie-btc.ts
@@ -10,5 +10,7 @@ startPolling(
   process.env.BET_AMOUNT,
   strategy,
   false,
-  PLATFORMS.CandleGenie
-);
+  PLATFORMS.CandleGenieBTC
+).catch((error) => {
+  console.error(error);
+});
---
+++ src/candlegenie-eth.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { parseStrategy, PLATFORMS, startPolling } from "./lib";
+
+dotenv.config();
+
+const strategy = parseStrategy(process.argv);
+
+startPolling(
+  process.env.PRIVATE_KEY,
+  process.env.BET_AMOUNT,
+  strategy,
+  false,
+  PLATFORMS.CandleGenieETH
+).catch((error) => {
+  console.error(error);
+});
